7ed935ec701ead15cb78e00b24809cfa34bed911
valerio <romavalerioio@gmail.com>
2021-11-27 18:27:43 +0000

    material ui table to render the list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { dealsApi } from "./api/deals";
 import Item from "./components/Item";
 import { useEffect, useState } from "react";
+import TableContainer from "@mui/material/TableContainer";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
 
 function App() {
   const [items, setItems] = useState([]);
@@ -11,15 +15,15 @@ function App() {
     fetchData();
   }, []);
   return (
-    <>
-      <ul>
-        {items.map((i) => (
-          <li>
-            <Item deal={i} />
-          </li>
-        ))}
-      </ul>
-    </>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableBody>
+          {items.map((item) => (
+            <Item deal={item} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,7 +1,10 @@
 import ItemDetails from "../ItemDetails";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
 
 function Item({
   deal: {
+    deal_id,
     provider_logo_image_url,
     provider_name,
     deal_name,
@@ -14,25 +17,40 @@ function Item({
   },
 }) {
   return (
-    <div>
-      <img src={provider_logo_image_url} alt={provider_name} />
-      <div>
-        <h4>{provider_name}</h4>
-        <p>{deal_name}</p>
-        <div>{provider_rating}</div>
-      </div>
-      <div>
+    <TableRow
+      key={deal_id}
+      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+    >
+      <TableCell align="right">
+        <img src={provider_logo_image_url} alt={provider_name} />
+      </TableCell>
+      <TableCell align="right">
+        <div>
+          <h4>{provider_name}</h4>
+          <p>{deal_name}</p>
+          <div>{provider_rating}</div>
+        </div>
+      </TableCell>
+      <TableCell align="right">
         <h4>£{monthly_price}</h4>
         <p>Monthly cost</p>
-      </div>
-      <ItemDetails
-        data={`${internet_speed} Mbps`}
-        description={broadband_type}
-      />
-      <ItemDetails data={`£${set_up_cost}`} description="Setup Costs" />
-      <ItemDetails data={contract_info} description="Contract" />
-      <button>Add to compare</button>
-    </div>
+      </TableCell>
+      <TableCell align="right">
+        <ItemDetails
+          data={`${internet_speed} Mbps`}
+          description={broadband_type}
+        />
+      </TableCell>
+      <TableCell align="right">
+        <ItemDetails data={`£${set_up_cost}`} description="Setup Costs" />
+      </TableCell>
+      <TableCell align="right">
+        <ItemDetails data={contract_info} description="Contract" />
+      </TableCell>
+      <TableCell align="right">
+        <button>Add to compare</button>
+      </TableCell>
+    </TableRow>
   );
 }
 
